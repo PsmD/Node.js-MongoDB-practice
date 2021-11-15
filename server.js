8a2050076ffcf9ee3833d3b7e8503908237455c4
PsmD <tbvjeld7@naver.com>
2021-11-16 00:28:11 +0900

    code 수정, model을 이용한 데이터 저장 코드 예정

--- server.js
+++ server.js
@@ -2,29 +2,22 @@ const express = require('express');
 const app = express();
 const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}));
-const MongoClient = require('mongodb').MongoClient;
-
-var db;
-MongoClient.connect('mongodb+srv://dragon:ehrhs431@cluster0.02ouy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', (err, client)=>{
-if(err) return console.log(err)    
-db = client.db('diaryapp');
-    console.log('저장완료');
-            app.listen(5000, ()=>{
-                console.log('http://localhost:5000')
-    });
-    
-});
+import Diary from './models/Diary';
 
 
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/home.html')
+    res.sendFile(__dirname + '/views/home.html')
 });
 app.get('/write', (req, res)=>{
-    res.sendFile(__dirname + '/write.html')
+    res.sendFile(__dirname + '/views/write.html')
 });
 app.post('/add', (req, res)=>{
     res.send('전송완료');
-    db.collection('post').insertOne({제목: req.body.title, 날짜: req.body.date}, ()=>{
+    db.collection('post').insertOne({title: req.body.title, issues: req.body.issues}, ()=>{
         console.log('저장완료')
     });
+});
+
+app.listen(5000, ()=>{
+    console.log('http://localhost:5000')
 });
