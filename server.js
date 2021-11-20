c33c9ac6e6611ad4e26eaf88ab5869076d33a044
PsmD <tbvjeld7@naver.com>
2021-11-20 22:41:16 +0900

    server 역할 init으로 분리

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-import "./db";
-require("dotenv").config();
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({extended: true})) 
-app.set('view engine', 'ejs');
-
-
-app.listen(process.env.PORT, ()=>{
-    console.log('http://localhost:5000')
-});
 
 import rootRouter from "./routers/rootRouter";
+
+app.use(express.urlencoded({extended: true})) 
+app.set('view engine', 'ejs');
 app.use("/", rootRouter);
+
 export default app;
