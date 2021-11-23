48244aba6707cf106e8b2d27cfbbf973d5c14f1d
PsmD <tbvjeld7@naver.com>
2021-11-23 22:09:58 +0900

    import module, middleware 추가

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+import rootRouter from "./routers/rootRouter";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import { localsMiddleware } from "./middlewares";
+
 const express = require('express');
 const app = express();
 
-import rootRouter from "./routers/rootRouter";
-
-app.use(express.urlencoded({extended: true})) 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true})) 
+app.use(
+    session({
+      secret: process.env.COOKIE_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+    })
+  );
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 
 export default app;
