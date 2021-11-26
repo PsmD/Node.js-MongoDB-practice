760dc8fb7a621a8328c0e03d21af257315deae5b
PsmD <tbvjeld7@naver.com>
2021-11-26 21:30:38 +0900

    watch, upload router 추가

--- routers/rootRouter.js
+++ routers/rootRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { getJoin, postJoin, cart, cs ,getLogin, logout, postLogin, getmypage, getChangePassword, postChangePassword, postmypage, track, viewed, home } from "../controllers/userController";
-import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
+import { getUpload, postUpload, getJoin, postJoin, cart, cs ,getLogin, logout, postLogin, getmypage, getChangePassword, postChangePassword, postmypage, track, viewed, home, watch } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware, imageUpload } from "../middlewares";
 
 const rootRouter = express.Router();
 
@@ -23,5 +23,9 @@ rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
 rootRouter.route("/login").all(publicOnlyMiddleware).get(getLogin).post(postLogin);
 
 rootRouter.get("/logout", protectorMiddleware, logout);
+
+rootRouter.get("/:id([0-9a-f]{24})", watch);
+
+rootRouter.route("/upload").all(protectorMiddleware).get(getUpload).post(imageUpload.single("image"), postUpload);
 
 export default rootRouter;
