f8a9f065195a6a2be8b0b2e6d0b314f38a037c98
PsmD <tbvjeld7@naver.com>
2021-11-26 21:34:42 +0900

    img model

---
+++ models/Image.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const imageSchema = new mongoose.Schema({
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  fileUrl: { type: String, required: true },
+  description: { type: String, required: true, trim: true, minLength: 20 },
+});
+
+const Image = mongoose.model("Image", imageSchema);
+
+export default Image;
