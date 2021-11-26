99f57629c31efd52e8c042384baf32cfd982ddc9
PsmD <tbvjeld7@naver.com>
2021-11-26 21:25:20 +0900

    multer 추가

--- middlewares.js
+++ middlewares.js
@@ -1,3 +1,5 @@
+import multer from "multer";
+
 export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.loggedInUser = req.session.user;
@@ -19,3 +21,10 @@ export const localsMiddleware = (req, res, next) => {
       return res.redirect("/");
     }
   };
+
+  export const imageUpload = multer({
+    dest: "uploads/images/",
+    limits: {
+      fileSize: 10000000,
+    },
+  });
